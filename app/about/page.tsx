a06e29d87333e3d44d1b1e980764e236930e436e
Karthick1242004 <karthick@thecloud.company>
2025-12-18 17:14:31 +0530

    Updated About page to consolidate branding by changing "HEX and HUE" to "Hex&Hue".

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -117,8 +117,7 @@ export default function AboutPage() {
                     animate={contentInView ? { opacity: 1, y: 0 } : {}}
                     transition={{ duration: 0.8, delay: 0.2 }}
                   >
-                    HEX{" "}and{" "}
-                    HUE
+                    Hex&Hue
                   </motion.h2>
                   
                   <motion.div
